803c473507dd7495f25b887962b2837f648738e6
JD <me@jdinh.life>
2017-02-08 14:29:46 +1100

    code splitting with es2015 import instead of require.ensure now that we're using webpack 2

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -22,7 +22,8 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['es2015', 'react', 'stage-0']
+                            presets: ['es2015', 'react', 'stage-0'],
+                            plugins: ['transform-object-rest-spread']
                         }
                     }
                 ]
