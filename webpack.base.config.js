995354bf2647b6ee6232f58645dce2c74d3e9138
JD <me@jdinh.life>
2017-02-09 09:29:03 +1100

    disabled modules transformation to potentially improve tree-shaking

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -22,7 +22,7 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['es2015', 'react', 'stage-0'],
+                            presets: [['es2015', { modules: false }], 'react', 'stage-0'],
                             plugins: ['transform-object-rest-spread']
                         }
                     }
